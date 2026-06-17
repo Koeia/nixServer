061fff512d9b011e5241627d677fc7dbbc79a099
Koeia <whereischason@protonmail.com>
2026-06-17 00:46:45 -0400

    Update home.nix

--- home.nix
+++ home.nix
@@ -26,7 +26,7 @@
       enable = true;
     };
     shellAliases = {
-      update = "~/nixos-dotfiles/Scripts/update.sh";
+      update = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#nixServer";
     };
     initContent = ''
       		${pkgs.fastfetch}/bin/fastfetch
